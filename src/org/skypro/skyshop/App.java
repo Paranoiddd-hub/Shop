ea45384aec9cca92164b93d0de58a3c54eef8e1b
Aidar <zeekacoola@gmail.com>
2025-11-17 23:22:18 +0300

    Домашка Java Collections Framework: List

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -20,6 +20,7 @@ public class App {
 
         ProductBasket basket = new ProductBasket();
         // 1. Добавление продукта в корзину.
+        System.out.println("1. Добавление продукта в корзину");
         basket.addProduct(chicken);
         basket.addProduct(butter);
         basket.addProduct(bread);
@@ -27,35 +28,45 @@ public class App {
         basket.addProduct(buckwheat);
         System.out.println();
         // 2. Добавление продукта в заполненную корзину, в которой нет свободного места.
+        System.out.println("2. Добавление продукта в заполненную корзину, в которой нет свободного места.");
         basket.addProduct(milk);
         System.out.println();
         // 3. Печать содержимого корзины с несколькими товарами.
+        System.out.println("3. Печать содержимого корзины с несколькими товарами.");
         basket.printInfo();
         System.out.println();
         // 4. Получение стоимости корзины с несколькими товарами.
+        System.out.println("4. Получение стоимости корзины с несколькими товарами.");
         System.out.println(basket.totalCost());
         System.out.println();
         // 5. Поиск товара, которого нет в корзине.
+        System.out.println("5. Поиск товара, которого нет в корзине.");
         System.out.println(basket.contain("eggs"));
         System.out.println();
         // 6. Поиск товара, которого нет в корзине.
+        System.out.println("6. Поиск товара, которого нет в корзине.");
         System.out.println(basket.contain("milk"));
         System.out.println();
         // 7. Очистка корзины.
+        System.out.println(" 7. Очистка корзины.");
         basket.clear();
-        System.out.println(Arrays.toString(basket.getBasket()));
+        System.out.println(basket.getBasket());
         System.out.println();
         // 8. Печать содержимого пустой корзины.
+        System.out.println("8. Печать содержимого пустой корзины.");
         basket.printInfo();
         System.out.println();
         // 9. Получение стоимости пустой корзины.
+        System.out.println("9. Получение стоимости пустой корзины.");
         System.out.println(basket.totalCost());
         System.out.println();
         // 10. Поиск товара по имени в пустой корзине.
+        System.out.println("10. Поиск товара по имени в пустой корзине.");
         System.out.println(basket.contain("icecream"));
         System.out.println();
         // Домашка ООП.Полиморфизм.Интерфейсы
-        SearchEngine searchEngine = new SearchEngine(8);
+        System.out.println("Домашка ООП.Полиморфизм.Интерфейсы");
+        SearchEngine searchEngine = new SearchEngine();
 
         searchEngine.add(chicken);
         searchEngine.add(butter);
@@ -69,11 +80,12 @@ public class App {
         searchEngine.add(article1);
         searchEngine.add(article2);
 
-        System.out.println(Arrays.toString(searchEngine.getSearchBase()));
-        System.out.println(Arrays.toString(searchEngine.search("хлеб")));
+        System.out.println(searchEngine.getSearchBase());
+        System.out.println(searchEngine.search("хлеб"));
         System.out.println();
 
         // Домашка Исключения в Java.
+        System.out.println("Домашка Исключения в Java.");
         System.out.println("         Исключения в Java");
         try {
             Product testProduct1 = new SimpleProduct(" ", 30);
@@ -99,5 +111,26 @@ public class App {
         } catch (BestResultNotFound e) {
             System.out.println(e);
         }
+        System.out.println();
+
+        // Домашка Java Collections Framework: List
+        System.out.println("Домашка Java Collections Framework: List");
+        basket.addProduct(chicken);
+        basket.addProduct(butter);
+        basket.addProduct(bread);
+        basket.addProduct(eggs);
+        basket.addProduct(buckwheat);
+        System.out.println(basket.deleteProduct("курица"));
+        basket.printInfo();
+        System.out.println();
+
+        basket.printInfo();
+        System.out.println();
+
+        System.out.println(basket.deleteProduct("молоко") + " Список пуст!");
+        System.out.println();
+
+        basket.printInfo();
+
     }
 }
